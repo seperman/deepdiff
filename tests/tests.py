ce7ae095f249ba8ed13836f63dbb3e7be8a44107
seperman <sdehpour@truecar.com>
2015-12-14 16:04:53 -0800

    Fixing a test regarding ignoring orders with list of unhashables.

--- tests/tests.py
+++ tests/tests.py
@@ -122,8 +122,14 @@ class DeepDiffTestCase(unittest.TestCase):
         t1 = [1, {"a": 2}, {"b": [3, 4, {1: 1}]}, "B"]
         t2 = [{"b": [3, 4, {1: 1}]}, {"a": 2}, {1: 1}]
         ddiff = DeepDiff(t1, t2, ignore_order=True)
-        self.assertEqual(ddiff, {'iterable_item_removed': ["root: [1, 'B']"],
-                                 'iterable_item_added': ['root: [{1: 1}]']})
+        try:
+            self.assertEqual(ddiff, {'iterable_item_removed': ["root: [1, 'B']"],
+                                     'iterable_item_added': ['root: [{1: 1}]']})
+        # sometimes it comes as ['B', 1] instead of [1, 'B'].
+        # We don't have control over that since we are ignoring order.
+        except:
+            self.assertEqual(ddiff, {'iterable_item_removed': ["root: ['B', 1]"],
+                                     'iterable_item_added': ['root: [{1: 1}]']})
 
     def test_list_that_contains_dictionary(self):
         t1 = {1: 1, 2: 2, 3: 3, 4: {"a": "hello", "b": [1, 2, {1: 1, 2: 2}]}}
