4301ff2ef6fa5fedcca9d25558d06325a96443d2
Bernhard Thiel <thiel@tbi.univie.ac.at>
2016-11-09 11:32:44 +0100

    Added 2 failing tests for the combination of significant_digits with sets.
    Sets use hasing for the diff, thus `__diff_numbers` is currently not invoked,
    which is why the tests currently fail.

--- tests/test_diff_ref.py
+++ tests/test_diff_ref.py
@@ -133,6 +133,18 @@ class DeepDiffRefTestCase(unittest.TestCase):
         self.assertEqual(change.path(force='yes'), 'root(unrepresentable)')
         self.assertEqual(change.path(force='fake'), 'root[2]')
 
+    def test_significant_digits(self):
+        ddiff = DeepDiff([0.012, 0.98], [0.013, 0.99], significant_digits = 1)
+        self.assertEqual(ddiff, {})
+        
+    def test_significant_digits_with_sets(self):
+        ddiff = DeepDiff(set([0.012, 0.98]), set([0.013, 0.99]), significant_digits = 1)
+        self.assertEqual(ddiff, {})
+        
+    def test_significant_digits_with_ignore_order(self):
+        ddiff = DeepDiff([0.012, 0.98], [0.013, 0.99], significant_digits = 1, ignore_order=True)
+        self.assertEqual(ddiff, {})
+
 class DeepDiffRefWithNumpyTestCase(unittest.TestCase):
 
     """DeepDiff Tests."""
