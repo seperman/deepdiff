e6b445de34150768d95cb7f6cdd4c664eb49ed93
Sep Dehpour <sep@zepworks.com>
2017-02-09 16:43:59 -0800

    Adding py 3.6

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(name='deepdiff',
           "Programming Language :: Python :: 3.3",
           "Programming Language :: Python :: 3.4",
           "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
           "Programming Language :: Python :: Implementation :: PyPy",
           "Development Status :: 5 - Production/Stable",
           "License :: OSI Approved :: MIT License"
