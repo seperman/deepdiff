a53d874f49176839737682e5289484477262243d
Sep Dehpour <sep@zepworks.com>
2019-02-18 00:28:39 -0800

    changing test runner

--- run_tests.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import unittest
-loader = unittest.TestLoader()
-tests = loader.discover('.')
-testRunner = unittest.runner.TextTestRunner()
-testRunner.run(tests)
