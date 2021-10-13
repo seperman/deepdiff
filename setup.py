ba10371918aa9066a5219efd1b12a1f5f4fc4444
Seperman <sep@zepworks.com>
2021-10-12 22:14:01 -0700

    Bump version: 5.5.0 → 5.6.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.version_info.major == 2:  # pragma: no cover
 if os.environ.get('USER', '') == 'vagrant':
     del os.link
 
-version = '5.5.0'
+version = '5.6.0'
 
 
 def get_reqs(filename):
