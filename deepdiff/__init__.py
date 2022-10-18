11390f08714f278724783f8207b4f997f0218304
Seperman <sep@zepworks.com>
2022-10-17 17:47:36 -0700

    Bump version: 6.2.0 → 6.2.1

--- deepdiff/__init__.py
+++ deepdiff/__init__.py
@@ -1,6 +1,6 @@
 """This module offers the DeepDiff, DeepSearch, grep, Delta and DeepHash classes."""
 # flake8: noqa
-__version__ = '6.2.0'
+__version__ = '6.2.1'
 import logging
 
 if __name__ == '__main__':
--- docs/index.rst
+++ docs/index.rst
@@ -4,7 +4,7 @@
    contain the root `toctree` directive.
 
 
-DeepDiff 6.2.0 documentation!
+DeepDiff 6.2.1 documentation!
 =============================
 
 *****************
