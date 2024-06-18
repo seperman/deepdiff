d84e893e451973f9bb5745a0ff8a275a32190655
stebranchi <30404479+stebranchi@users.noreply.github.com>
2024-06-18 12:50:26 +0200

    Updating version to 8.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.version_info.major == 2:  # pragma: no cover
 if os.environ.get('USER', '') == 'vagrant':
     del os.link
 
-version = '8.0.0'
+version = '8.0.1'
 
 
 def get_reqs(filename):
