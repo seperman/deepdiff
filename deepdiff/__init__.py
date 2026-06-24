f48b9bdd9097045245a81510af533815b06ba499
Sep Dehpour <sep@zepworks.com>
2026-06-24 12:08:01 +0200

    9.1.0

--- deepdiff/__init__.py
+++ deepdiff/__init__.py
@@ -1,6 +1,6 @@
 """This module offers the DeepDiff, DeepSearch, grep, Delta and DeepHash classes."""
 # flake8: noqa
-__version__ = '9.0.0'
+__version__ = '9.1.0'
 import logging
 
 if __name__ == '__main__':
--- deepdiff/docstrings/index.rst
+++ deepdiff/docstrings/index.rst
@@ -4,7 +4,7 @@
    contain the root `toctree` directive.
 
 
-DeepDiff 9.0.0 documentation!
+DeepDiff 9.1.0 documentation!
 =============================
 
 .. |qluster_link| raw:: html
